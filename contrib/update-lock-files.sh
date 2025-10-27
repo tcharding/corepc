b7612eb8766b2dc6f84bede1c0a658c5f4958d2d
Tobin C. Harding <me@tobin.cc>
2025-10-28 09:17:36 +1100

    Fix update-lock-files script
    Fix and run the `update-lock-files.sh` script to correctly handle all
    the crates in this repo.
    
    Notes:
    
    - `electrsd` needs some features
    - The rest can be built with `cargo check`
    
    (I'm not 100% sure on the validity of these notes.)

--- contrib/update-lock-files.sh
+++ contrib/update-lock-files.sh
@@ -6,6 +6,12 @@ set -euo pipefail
 
 for file in Cargo-minimal.lock Cargo-recent.lock; do
     cp --force "$file" Cargo.lock
-    cargo check --all-features
+
+    cd electrsd
+    cargo check --features="corepc-node_29_0,electrs_0_10_6"
+    cd ..
+
+    cargo check
+
     cp --force Cargo.lock "$file"
 done
