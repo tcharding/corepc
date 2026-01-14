7756c423b4f44d56447d17f01dd68bc764a21889
RenatoBrittoAraujo <renatobritto.professional@gmail.com>
2026-01-14 15:12:43 -0300

    fix(mining): correct docs API version to v30

--- types/src/v30/mining/mod.rs
+++ types/src/v30/mining/mod.rs
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: CC0-1.0
 
-//! The JSON-RPC API for Bitcoin Core `v29` - mining.
+//! The JSON-RPC API for Bitcoin Core `v30` - mining.
 //!
 //! Types for methods found under the `== Mining ==` section of the API docs.
 
