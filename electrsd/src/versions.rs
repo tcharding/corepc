dc7dccf1c3c45ae585f42ed86cc298d105b354e3
Renato Britto <renatobritto@protonmail.com>
2026-05-13 18:43:42 -0300

    Include electrsd in workspace

--- electrsd/src/versions.rs
+++ electrsd/src/versions.rs
@@ -1,3 +1,14 @@
+// An explicit version of Bitcoin Core must be selected by enabling some feature.
+// We check this here instead of in `lib.rs` because this file is included in `build.rs`.
+#[cfg(not(any(
+    feature = "electrs_0_8_10",
+    feature = "electrs_0_9_1",
+    feature = "electrs_0_9_11",
+    feature = "electrs_0_10_6",
+    feature = "esplora_a33e97e1",
+)))]
+compile_error!("enable a feature in order to select the version of electrs to use");
+
 #[cfg(target_os = "macos")]
 const OS: &str = "macos";
 
@@ -23,6 +34,8 @@ const VERSION: &str = "v0.8.10";
 #[cfg(all(feature = "esplora_a33e97e1", not(feature = "all_features")))]
 const VERSION: &str = "esplora_a33e97e1a1fc63fa9c20a116bb92579bbf43b254";
 
+/// This is meaningless but we need it otherwise we can't get far enough into
+/// the build process to trigger the `compile_error!` in `./versions.rs`.
 #[cfg(not(any(
     feature = "electrs_0_8_10",
     feature = "electrs_0_9_1",
@@ -30,14 +43,6 @@ const VERSION: &str = "esplora_a33e97e1a1fc63fa9c20a116bb92579bbf43b254";
     feature = "electrs_0_10_6",
     feature = "esplora_a33e97e1",
 )))]
-const VERSION: &str = "NA";
-
-pub const HAS_FEATURE: bool = cfg!(any(
-    feature = "electrs_0_8_10",
-    feature = "electrs_0_9_1",
-    feature = "electrs_0_9_11",
-    feature = "electrs_0_10_6",
-    feature = "esplora_a33e97e1",
-));
+const VERSION: &str = "never-used";
 
 pub fn electrs_name() -> String { format!("electrs_{}_{}", OS, VERSION) }
